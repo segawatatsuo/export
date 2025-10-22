d4b3abf4d42f893cce57f07fcee14a365ac98d2d
segawatatsuo <base@lookingfor.jp>
2025-10-22 17:39:18 +0900

    10-22 17:39

--- resources/views/order_upload.blade.php
+++ resources/views/order_upload.blade.php
@@ -31,7 +31,11 @@
                 <div class="card">
                     <div class="card-header"></div>
                     <div class="card-body">
+                        <!--
                         <object data="{{ asset('storage/order') . '/' . $file_name }}" width="900" height="900"></object>
+                        -->
+                        <object data="{{ Storage::url('order/' . $file_name) }}" width="900" height="900"></object>
+
                     </div>
                 </div>
                 <br>
